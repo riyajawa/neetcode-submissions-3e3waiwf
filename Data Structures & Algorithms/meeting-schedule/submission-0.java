83c0047155ba97d4fc30d5febbce5fc3d2aa83be
riya <riya1119.cse19@chitkara.edu.in>
2026-05-01 17:08:30 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.java
@@ -0,0 +1,23 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        intervals.sort((a,b) -> a.start - b.start); // sort
+
+        for(int i=1;i<intervals.size();i++){
+            if(intervals.get(i).start < intervals.get(i-1).end){
+                return false;
+            }
+        }
+        return true;
+    }
+}
